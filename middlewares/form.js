ff46520355b7886e7d55032a5248854d8251309b
Dimas <dimasriat@gmail.com>
2020-06-28 17:57:25 +0700

    login page udah bener tinggal sessionnya

--- middlewares/form.js
+++ middlewares/form.js
@@ -1,5 +1,6 @@
 const { check, validationResult, body } = require("express-validator");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 exports.postSubmit = [
 	check("title", "title cannot be blank").exists(),
@@ -7,16 +8,26 @@ exports.postSubmit = [
 ];
 
 exports.loginSubmit = [
-	check("username", "username cannot be blank").exists(),
-	check("password", "password cannot be blank").exists(),
+	check("username", "username cannot be blank").not().isEmpty(),
+	check("password", "password cannot be blank").not().isEmpty(),
+	body("username").custom(async (username, { req }) => {
+		const found = await User.findOne({ where: { username } });
+		if (!found) return Promise.reject("username or password is wrong");
+		const accepted = await bcrypt.compare(
+			req.body.password,
+			found.password
+		);
+		if (!accepted) return Promise.reject("username or password is wrong");
+	}),
 ];
 
 exports.registerSubmit = [
 	check("username", "username cannot be blank").exists(),
-	body("username").custom((username) => {
-		return User.findOne({ where: { username } }).then((found) => {
-			return found && Promise.reject("username has been taken");
-		});
+	body("username").custom(async (username) => {
+		const found = await User.findOne({ where: { username } });
+		return found
+			? Promise.reject("username has been taken")
+			: Promise.resolve();
 	}),
 	check("email", "email cannot be blank").exists(),
 	check("email", "email are not valid").isEmail(),
--- models/User.js
+++ models/User.js
@@ -1,23 +1,26 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../utils/dbinit");
 
-const User = sequelize.define("User", {
-	username: {
-		type: DataTypes.STRING,
-		unique: true,
-		allowNull: false,
+const User = sequelize.define(
+	"User",
+	{
+		username: {
+			type: DataTypes.STRING,
+			unique: true,
+			allowNull: false,
+		},
+		email: {
+			type: DataTypes.STRING,
+			unique: true,
+			allowNull: false,
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		resetPasswordToken: DataTypes.STRING,
+		resetPasswordExpires: DataTypes.DATE,
 	},
-	email: {
-		type: DataTypes.STRING,
-		unique: true,
-		allowNull: false,
-	},
-	password: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	resetPasswordToken: DataTypes.STRING,
-	resetPasswordExpires: DataTypes.DATE,
-});
+);
 
 module.exports = User;
