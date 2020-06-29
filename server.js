8089d806ee73c6ba95d7652bd44b5a5d76ee6969
Dimas <dimasriat@gmail.com>
2020-06-29 13:37:01 +0700

    mockup template udah jadi

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use("/assets", express.static(`${__dirname}/public`));
 // const indexRouter = require("./routers/index");
 // app.use("/", indexRouter);
 
-const userRouter = require("./routers/user");
-app.use("/user", userRouter);
+// const userRouter = require("./routers/user");
+// app.use("/user", userRouter);
+
+/**TEST ROUTER
+ * digunakan untuk mendebug router
+ */
+app.get("/", (req, res) => res.render("index"));
 
 app.listen(8000, () => console.log("server is running at port 8000"));
