0e1c0489157a791651907a525c31e07fa982b01d
Dimas <dimasriat@gmail.com>
2020-07-07 19:57:56 +0700

    view udah 30%

--- routers/index.router.js
+++ routers/index.router.js
@@ -1,75 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const { validationResult } = require("express-validator");
 
-const { Admin, About } = require("../models/index.model");
-const bcrypt = require("bcryptjs");
-
-const FORM = require("../middlewares/form.middleware");
 const AUTH = require("../middlewares/auth.middleware");
 
-const fs = require("fs");
-const path = require("path");
-
-const fs_rename = (x, y) =>
-	new Promise((resolve) => {
-		fs.rename(x, y, () => resolve());
-	});
-
-router.get("/", AUTH["BOTH"], async (req, res) => {
-	try {
-		const { id } = await Admin.findOne({
-			where: { year: "2020" },
-		});
-		const about = await About.findOne({
-			where: {
-				adminId: id,
-			},
-		});
-		console.log(JSON.stringify(await About.findAll(), null, 2));
-		res.render("test/index", { about });
-	} catch (err) {
-		console.error(err);
-	}
+router.get("/", AUTH["BOTH"], (req, res) => {
+	return res.render("index");
 });
-router.get("/about", AUTH["BOTH"], async (req, res) => {
-	try {
-		const { id } = await Admin.findOne({
-			where: { year: "2020" },
-		});
-		const about = await About.findOne({
-			where: {
-				adminId: id,
-			},
-		});
-		res.render("test/about", { about });
-	} catch (err) {
-		console.error(err);
-	}
+router.get("/profil", AUTH["BOTH"], (req, res) => {
+	return res.render("profil");
 });
-router.get("/event", AUTH["BOTH"], (req, res) => res.render("test/event"));
-router.get("/gallery", AUTH["BOTH"], (req, res) => res.render("test/gallery"));
-router.get("/team", AUTH["BOTH"], (req, res) => res.render("test/team"));
-router.get("/login", AUTH["NOT_LOGGED_ONLY"], (req, res) =>
-	res.render("test/login")
-);
-router.post(
-	"/login",
-	AUTH["NOT_LOGGED_ONLY"],
-	FORM["LOGIN_SUBMIT"],
-	(req, res) => {
-		const error = validationResult(req);
-		if (error.isEmpty()) {
-			res.redirect("/admin");
-		} else {
-			req.flash("login", error.array());
-			const err = req
-				.flash("login")
-				.map((item) => `<p>${item.msg}</p>`)
-				.join("");
-			res.send(err);
-		}
-	}
-);
+router.get("/event", AUTH["BOTH"], (req, res) => {
+	return res.render("event");
+});
+router.get("/event/:eventId", AUTH["BOTH"], (req, res) => {
+	return res.render("event-view");
+});
+router.get("/team", AUTH["BOTH"], (req, res) => {
+	return res.render("team");
+});
+router.get("/team/:teamId", AUTH["BOTH"], (req, res) => {
+	return res.render("team-view");
+});
+router.get("/gallery", AUTH["BOTH"], (req, res) => {
+	return res.render("gallery");
+});
+router.get("/gallery/:galleryId", AUTH["BOTH"], (req, res) => {
+	return res.render("gallery-view");
+});
+router.get('/login', AUTH['NOT_LOGGED_ONLY'], (req, res) => {
+	return res.render('login');
+})
 
 module.exports = router;
